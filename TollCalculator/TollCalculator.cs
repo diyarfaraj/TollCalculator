cdfe3ac2e4c3b1dff56ca4c94eec8ee207e9d482
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-07 02:45:25 +0200

    refactor get time between intervals

--- TollCalculator/TollCalculator.cs
+++ TollCalculator/TollCalculator.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 using TollCalculator.Interface;
 
 namespace TollCalculator
 {
     public class TollCalculator
     {
-
+        private readonly IHolidayService _holidayService;
+        public TollCalculator(IHolidayService holidayService)
+        {
+            _holidayService = holidayService;
+        }
         /**
          * Calculate the total toll fee for one day
          *
@@ -15,7 +21,7 @@ namespace TollCalculator
          * @return - the total toll fee for that day
          */
 
-        public int GetTollFee(IVehicle vehicle, DateTime[] dates)
+        public int GetVehicleTollFee(IVehicle vehicle, DateTime[] dates)
         {
             DateTime intervalStart = dates[0];
             int totalFee = 0;
@@ -42,70 +48,51 @@ namespace TollCalculator
             return totalFee;
         }
 
-        private bool IsTollFreeVehicle(IVehicle vehicle)
-        {
-            if (vehicle == null) return false;
-            string vehicleType = vehicle.GetVehicleType();
-            return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
-                   vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
-                   vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
-                   vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
-                   vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
-                   vehicleType.Equals(TollFreeVehicles.Military.ToString());
-        }
-
         public int GetTollFee(DateTime date, IVehicle vehicle)
         {
-            if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
+            if (/*IsTollFreeDate(date) ||*/ vehicle.IsTollFree()) return 0;
 
-            int hour = date.Hour;
-            int minute = date.Minute;
+            if (IsBetween(date, "06:00", "06:29")) return 8;
+            if (IsBetween(date, "06:30", "06:59")) return 13;
+            if (IsBetween(date, "07:00", "07:59")) return 18;
+            if (IsBetween(date, "08:00", "08:29")) return 13;
+            if (IsBetween(date, "08:30", "14:59")) return 8;
+            if (IsBetween(date, "15:00", "15:29")) return 13;
+            if (IsBetween(date, "15:30", "16:59")) return 18;
+            if (IsBetween(date, "17:00", "17:59")) return 13;
+            if (IsBetween(date, "18:00", "18:29")) return 8;
+            if (IsBetween(date, "18:30", "05:59")) return 0;
 
-            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-            else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-            else return 0;
+            return 0;
+        }
+        private bool IsBetween(DateTime entryTime, string startTime, string endTime)
+        {
+            TimeSpan start = TimeSpan.Parse(startTime);
+            TimeSpan end = TimeSpan.Parse(endTime);
+            TimeSpan now = entryTime.TimeOfDay;
+
+            return (now >= start) && (now <= end);
         }
 
-        private bool IsTollFreeDate(DateTime date)
+        private async Task<bool> IsTollFreeDate(DateTime date)
         {
-            int year = date.Year;
-            int month = date.Month;
-            int day = date.Day;
-
-            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-            if (year == 2013)
+            try
             {
-                if (month == 1 && day == 1 ||
-                    month == 3 && (day == 28 || day == 29) ||
-                    month == 4 && (day == 1 || day == 30) ||
-                    month == 5 && (day == 1 || day == 8 || day == 9) ||
-                    month == 6 && (day == 5 || day == 6 || day == 21) ||
-                    month == 7 ||
-                    month == 11 && day == 1 ||
-                    month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+                var holidays = await _holidayService.GetHolidays();
+                var holidayDates = holidays.Select(day => day).ToList();
+                var daysBoforeHoliday = await _holidayService.GetDayBeforeHoliday();
 
-        private enum TollFreeVehicles
-        {
-            Motorbike = 0,
-            Tractor = 1,
-            Emergency = 2,
-            Diplomat = 3,
-            Foreign = 4,
-            Military = 5
+                if ((date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) ||
+                (holidayDates.Contains(date) || daysBoforeHoliday.Contains(date))) return true;
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                //Add proper exception handling
+                Console.WriteLine(e.Message);
+                return false;
+            }
         }
     }
 }
